14763fce49fcc7ac62eb483cd221c53f2d8cbf15
OleksandrBazhyn <olexandrbazhyn@gmail.com>
2025-01-26 17:47:13 +0200

    Implement send message function

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './app.css'
+import LongPolling from './longpolling';
+
 
 function App() {
 
     return (
         <div>
-
+            <LongPolling />
         </div>
     )
 }
